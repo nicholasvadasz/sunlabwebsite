ed6d906892c15b0f00c63440c8a7d5a2d4828ec8
nicholasvadasz <nicholas_vadasz@brown.edu>
2022-03-11 04:21:38 -0500

    MenuBar implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import MenuBar from "./components/MenuBar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MenuBar />
+      <img
+        src="http://www2.cs.uh.edu/~acheng/ProfCheng_Brown.jpg"
+        alt="sunlab"
+        className="sunlab"
+        style={{ objectFit: "cover", width: "100%", height: "1050px" }}
+      />
+      <h1>LOLOLOL</h1>
+      <h1>LOLOLOL</h1>
+      <h1>LOLOLOL</h1>
+      <h1>LOLOLOL</h1>
+      <h1>LOLOLOL</h1>
     </div>
   );
 }
---
+++ src/components/MenuBar.tsx
@@ -0,0 +1,114 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Button from "@mui/material/Button";
+import MenuItem from "@mui/material/MenuItem";
+
+const pages = ["Hours", "About", "Contact", "Guides"];
+
+const MenuBar = () => {
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
+    null
+  );
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  return (
+    <AppBar
+      color="transparent"
+      elevation={0}
+      sx={{ backdropFilter: "blur(20px)" }}
+    >
+      <Toolbar
+        disableGutters
+        sx={{
+          background: "rgba(0, 0, 0,0.3)",
+        }}
+      >
+        <img
+          src="/newlogo.png"
+          alt="logo"
+          style={{ width: "2%", padding: ".3%", marginLeft: ".4%" }}
+        />
+        <Box
+          sx={{
+            flexGrow: 1,
+            display: { xs: "none", md: "flex" },
+            justifyContent: "flex-end",
+            mr: "30px",
+          }}
+        >
+          {pages.map((page) => (
+            <Button
+              key={page}
+              onClick={handleCloseNavMenu}
+              sx={{
+                my: 2,
+                color: "black",
+                display: "block",
+                fontWeight: "bold",
+              }}
+            >
+              {page}
+            </Button>
+          ))}
+        </Box>
+        <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+          <IconButton
+            size="large"
+            aria-label="account of current user"
+            aria-controls="menu-appbar"
+            aria-haspopup="true"
+            onClick={handleOpenNavMenu}
+            color="inherit"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Menu
+            id="menu-appbar"
+            anchorEl={anchorElNav}
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "left",
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "left",
+            }}
+            open={Boolean(anchorElNav)}
+            onClose={handleCloseNavMenu}
+            sx={{
+              display: { xs: "block", md: "none" },
+            }}
+          >
+            {pages.map((page) => (
+              <MenuItem key={page} onClick={handleCloseNavMenu}>
+                <Typography textAlign="center">{page}</Typography>
+              </MenuItem>
+            ))}
+          </Menu>
+        </Box>
+        <Typography
+          variant="h6"
+          noWrap
+          component="div"
+          sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}
+        >
+          LOGO
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  );
+};
+export default MenuBar;
