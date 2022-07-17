da01fe81f6e52ec8345a50e199bf9a3af224bed4
nicholasvadasz <nicholas_vadasz@brown.edu>
2022-07-16 23:35:38 -0500

    feat: cleaning up number of guides

--- src/pages/GuidesPage.tsx
+++ src/pages/GuidesPage.tsx
@@ -12,11 +12,6 @@ function GuidesPage() {
           <GuideCard emoji="⊞" title="SSH w/ Windows" link="SSHWindows" />
           <GuideCard emoji="🍎" title="SSH w/ Apple" link="SSHApple" />
           <GuideCard emoji="🐧" title="SSH w/ Linux" link="SSHLinux" />
-          <GuideCard emoji="🪟" title="SSH w/ Windows" link="SSHWindows" />
-          <GuideCard emoji="🪟" title="SSH w/ Windows" link="SSHWindows" />
-          <GuideCard emoji="🪟" title="SSH w/ Windows" link="SSHWindows" />
-          <GuideCard emoji="🪟" title="SSH w/ Windows" link="SSHWindows" />
-          <GuideCard emoji="🪟" title="SSH w/ Windows" link="SSHWindows" />
         </div>
       </div>
     </div>
